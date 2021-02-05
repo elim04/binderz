a814504bafc456ea5e40ae5e24bbe76b7403a596
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-05 22:19:10 +0000

    topics

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,4 +3,4 @@ INSERT INTO users (name, email, password) VALUES ('Alice', 'aliceinwonderland@gm
 INSERT INTO users (name, email, password) VALUES ('Kira', 'kira@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 INSERT INTO users (name, email, password) VALUES ('Emily', 'ilovebbyoda@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 INSERT INTO users (name, email, password) VALUES ('Alvin', 'ngoalvin97@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (name, email, password) VALUES ('Armeen', 'armeen.hadizadeh@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+INSERT INTO users (name, email, password) VALUES ('Armeen', 'armeen.hadizadeh@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
---
+++ db/seeds/02_topics.sql
@@ -0,0 +1,12 @@
+// math = 1, comp sci = 2, french = 3, english = 4, science = 5, biology = 6, chemistry = 7, visual arts = 8, history = 9, physics = 10
+
+INSERT INTO topics (topic) VALUES ('math');
+INSERT INTO topics (topic) VALUES ('computer science');
+INSERT INTO topics (topic) VALUES ('french');
+INSERT INTO topics (topic) VALUES ('english');
+INSERT INTO topics (topic) VALUES ('science');
+INSERT INTO topics (topic) VALUES ('biology');
+INSERT INTO topics (topic) VALUES ('chemistry');
+INSERT INTO topics (topic) VALUES ('visual arts');
+INSERT INTO topics (topic) VALUES ('history');
+INSERT INTO topics (topic) VALUES ('physics');

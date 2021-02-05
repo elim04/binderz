74037a89a6565b320dd2fb11a6eb9cf3af15cbb2
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-05 22:16:32 +0000

    created db_schema and users/topics/resources/ratings/comments seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) VALUES ('Alice', 'aliceinwonderland@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Kira', 'kira@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Emily', 'ilovebbyoda@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Alvin', 'ngoalvin97@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Armeen', 'armeen.hadizadeh@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
---
+++ db/seeds/03_resources.sql
@@ -0,0 +1,28 @@
+// math = 1, comp sci = 2, french = 3, english = 4, science = 5, biology = 6, chemistry = 7, visual arts = 8, history = 9, physics = 10
+
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('Algebra Foundations', 'Khan Academy tutorial on algebra foundations', 2, 1, 'https://images.pexels.com/photos/53621/calculator-calculation-insurance-finance-53621.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'https://www.khanacademy.org/math/algebra/x2f8bb11595b61c86:foundation-algebra');
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('Making a modal', 'How to make a modal using html, css, and javascript', 1, 2, 'https://miro.medium.com/max/3576/1*VCl3Dz_jwsKOXUjLeD19VA.png', 'https://uxplanet.org/best-practices-for-modals-overlays-dialog-windows-c00c66cddd8c' );
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('5 Fun Physics Phenomena', '5 cool physics tricks but how do they work?', 1, 10, 'http://i3.ytimg.com/vi/1Xp_imnO6WE/maxresdefault.jpg', 'https://www.youtube.com/watch?v=1Xp_imnO6WE&ab_channel=Veritasium');
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('The Ice Cream Book', 'The science behind how ice cream is made', 3, 5, 'https://images.pexels.com/photos/1030941/pexels-photo-1030941.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'https://www.uoguelph.ca/foodscience/book-page/ice-cream-ebook');
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('The golden ratio', 'The golden math ratio', 3, 1, 'http://i3.ytimg.com/vi/sj8Sg8qnjOg/maxresdefault.jpg', 'https://www.youtube.com/watch?v=sj8Sg8qnjOg&ab_channel=Numberphile');
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('WW2 FACTS', 'BBCs history guide on WW2', 4, 9, 'https://images.pexels.com/photos/3007370/pexels-photo-3007370.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'http://www.bbc.co.uk/history/worldwars/wwtwo');
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('Colour Theory', 'The fundamentals of understanding color theory', 3, 8, 'https://images.pexels.com/photos/3517899/pexels-photo-3517899.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'https://99designs.ca/blog/tips/the-7-step-guide-to-understanding-color-theory/');
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('The Fermi Paradox', 'The universe is unbelievably big – trillions of stars and even more planets. Soo… there just has to be life out there, right? But where is it? Why don’t we see any aliens? Where are they? And more importantly, what does this tell us about our own fate in this gigantic and scary', 1, 10, 'http://i3.ytimg.com/vi/sNhhvQGsMEc/hqdefault.jpg', 'https://www.youtube.com/watch?v=sNhhvQGsMEc&ab_channel=Kurzgesagt%E2%80%93InaNutshell');
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('Advanced physics', 'Advanced physics', 1, 10, 'https://images-na.ssl-images-amazon.com/images/I/512yEu6OOPL._SX346_BO1,204,203,200_.jpg', 'https://www.youtube.com/watch?v=8mi0PoPvLvs');
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('Basic chemistry', 'Basic intro to chemistry', 2, 7, 'https://media.proprofs.com/images/QM/user_images/2503852/New%20Project%20(36)(86).jpg', 'https://www.youtube.com/watch?v=MYuh5yErdfA');
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('Basic biology', 'Basic intro to biology', 2, 6, 'https://i0.wp.com/basicbiology.net/wp-content/uploads/2018/05/cover.jpg?fit=575%2C719&ssl=1', 'https://www.youtube.com/watch?v=3w1fY67dnFI');
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('Basic French', 'Basic into to french', 3, 3, 'https://www.wikihow.com/images/thumb/a/a9/Speak-Basic-French-Step-4-Version-2.jpg/v4-460px-Speak-Basic-French-Step-4-Version-2.jpg.webp', 'https://www.youtube.com/watch?v=ujDtm0hZyII');
+INSERT INTO resources (title, description, user_id, topic_id, image_src, url)
+VALUES ('Physics Basics', 'Basic intro to physics', 1, 10, 'https://thumbs.dreamstime.com/b/learn-study-inspect-physics-pictured-as-magnifying-glass-enlarging-word-symbolizes-researching-exploring-analyzing-meaning-164571032.jpg', 'https://www.youtube.com/watch?v=b1t41Q3xRM8');

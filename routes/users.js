2632f9d9389640c8f5de1731b1ed35a195426854
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-06 20:33:04 +0000

    added all basic routes currently does not include bcrypt

--- routes/users.js
+++ routes/users.js
@@ -4,35 +4,79 @@
  *   these routes are mounted onto /users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
-
+// const bcrypt = require('bcrypt');
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get('/', (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
+  router.get('/me', (req, res) => {
+    const userId = req.session.userId;
+    if (!userId) {
+      res.send({message: "not logged in"});
+      return;
+    }
+
+    db.getUserWithId(userId)
+      .then(user => {
+        if (!user) {
+          res.send({error: "no user with that id"});
+          return;
+        }
+
+        res.send({user: {name: user.name, email: user.email, id: userId}});
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+      .catch(e => res.send(e));
   });
 
-  const register = function(user) {
-    return db.addUser(user);
-  };
+  // ------------------ REGISTER ROUTES ---------------------
 
   router.post('/register', (req, res) => {
     // const {name, email, password} = req.body;
     const newUserInfo = req.body;
-    register(newUserInfo);
-
+    db.addUser(newUserInfo)
+    .then(user => {
+      if (!user) {
+        res.send({error: 'user not created'});
+        return;
+      }
+      req.session.userId = user.id;
+      res.send("🤗");
+    })
+    .catch(e => res.send(e));
   });
 
+
+  // ----------------- LOG IN/OUT ROUTES --------------------
+
+  const login =  function(email, password) {
+    return db.getUserWithEmail(email)
+    .then(user => {
+      // if (bcrypt.compareSync(password, user.password)) {
+      if (password === user.password) {
+        return user;
+      }
+      return null;
+    });
+  }
+
+  router.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    login(email, password)
+      .then(user => {
+        if (!user) {
+          res.send({error: "error"});
+          return;
+        }
+        req.session.userId = user.id;
+        res.send({user: {name: user.name, email: user.email, id: user.id}});
+      })
+      .catch(e => res.send(e));
+  });
+
+  router.post('/logout', (req, res) => {
+    req.session.userId = null;
+    res.send({});
+  });
 
   return router;
 };

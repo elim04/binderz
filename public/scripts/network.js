ac4f520ba4502148a331912784660684573eb284
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-08 17:39:27 +0000

    add logIn ajax

--- public/scripts/network.js
+++ public/scripts/network.js
@@ -1,7 +1,15 @@
 const signUp = function (data) {
   return $.ajax({
     method:"POST",
-    url: "/api/users/me",
+    url: "/api/users/register",
+    data
+  });
+}
+
+const logIn = function(data) {
+  return $.ajax({
+    method:"POST",
+    url: "/api/users/login",
     data
   });
 }

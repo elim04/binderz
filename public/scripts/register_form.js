f82eeea00dca3f385337d64194f62b409d0118b7
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-08 20:59:36 +0000

    show errors and clears input/error upon close or successful registration which auto logs in

--- public/scripts/register_form.js
+++ public/scripts/register_form.js
@@ -1,11 +1,41 @@
 $(function () {
 
-  $("main").on('submit', '.modal-register-form', function(event){
+  const showRegisterError = function(message) {
+    $(".register_error").append(`<p>${message}</p>`);
+    $(".register_error").slideDown("slow", function() {});
+  }
+
+  const clearRegisterError = function() {
+    $(".register_error").empty();
+    $(".register_error").css('display', 'none');
+  }
+
+  const clearRegisterInput = function () {
+    $('.modal-register-content').find('input').each(function() {
+      $(this).val('');
+    })
+  }
+
+  $('main').on('submit', '.modal-register-form', function(event){
     event.preventDefault();
+    clearRegisterError();
     const data = $(this).serialize();
-    console.log(data);
     signUp(data)
+    .done(res => {
+      logIn(res)
+      $('.modal-bg4').removeClass('bg-active');
+      clearRegisterInput();
+    })
+    .fail(err => showRegisterError(err.responseText))
   });
 
+  $('.modal-bg4').on('focus','input', function() {
+    clearRegisterError();
+  })
 
-})
+  $('.modal-register-close').on('click', function() {
+    clearRegisterInput();
+    clearRegisterError();
+  })
+
+});

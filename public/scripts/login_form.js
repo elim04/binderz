fd91a163349bcd57adc979386fb38bb62da57031
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-08 20:58:55 +0000

    login form now shows error/clears all inputs when close/login successfully

--- public/scripts/login_form.js
+++ public/scripts/login_form.js
@@ -1,11 +1,42 @@
 $(function () {
 
+  const showLoginError = function(message) {
+    $('.login_error').append(`<p>${message}</p>`);
+    $('.login_error').slideDown("slow", function() {});
+  }
 
-$("main").on('submit', '.modal-login-form', function(event) {
-  event.preventDefault();
-  const data = $(this).serialize();
-  console.log(data);
-  logIn(data)
-});
+  const clearLoginError = function() {
+    $('.login_error').empty();
+    $('.login_error').css('display', 'none');
+  }
 
+  const clearLoginInput = function () {
+    $('.modal-login-content').find('input').each(function() {
+      $(this).val('');
+    })
+  }
+
+  $('main').on('submit', '.modal-login-form', function(event) {
+    event.preventDefault();
+    clearLoginError();
+    const data = $(this).serialize();
+    logIn(data)
+    .done(() => {
+      $('.modal-bg3').removeClass('bg-active');
+      clearLoginInput();
+    })
+    .fail(err => showLoginError(err.responseText))
+    });
+
+    $('.modal-bg3').on('focus','input', function() {
+      clearLoginError();
+    })
+
+
+
+  $('.modal-login-close').on('click', function() {
+    console.log($(this))
+    clearLoginInput();
+    clearLoginError();
+  })
 })

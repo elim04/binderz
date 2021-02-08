7981063290c4859ab0f95aeac2a8aa539cdcadc7
Emily Lim <emily.elim04@gmail.com>
2021-02-08 17:05:30 +0000

    add create_resource js file in scripts

--- public/scripts/network.js
+++ public/scripts/network.js
@@ -1,3 +1,13 @@
 function getAllResources() {
   console.log()
 }
+
+
+function addResource() {
+  console.log('Add resource');
+  return $.ajax({
+    method: "POST",
+    url: "/api/resources",
+    data: formContent,
+  });
+}

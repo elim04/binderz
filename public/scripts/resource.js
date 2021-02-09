ef02f3d737c3a42fb50a1be35f7e1919d6b4178b
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-09 15:57:59 +0000

    added a log in checker

--- public/scripts/resource.js
+++ public/scripts/resource.js
@@ -1,9 +1,19 @@
 $(function() {
 
-  $("#create-resource").on("click", function() {
-    console.log($(this))
-    $('.modal-bg2').addClass('bg-active');
-    $('.img-container').addClass('change-order')
+  $("#create-resource").on("click", async function() {
+    let isLoggedIn;
+    try {
+      isLoggedIn = await getMyDetails();
+    } catch(err) {
+      console.log(err, "error")
+    }
+    if (isLoggedIn) {
+      $('.modal-bg2').addClass('bg-active');
+      $('.img-container').addClass('change-order')
+    } else {
+      $('.modal-bg3').addClass('bg-active');
+      $('.img-container').addClass('change-order')
+    }
   })
 
   $(".modal-resource-close").on("click", function() {

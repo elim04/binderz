a65d7055736a3539ae537f3ca6b8b9b2175af99f
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-02-09 14:01:24 -0500

    public/scripts/home.js

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -28,6 +28,8 @@ $(function () {
     }
   }
 
+  window.masonaryResize = masonaryResize;
+
   const createResource = function (resource) {
     let resourceHTML = `
       <div class="block">
@@ -156,6 +158,8 @@ $(function () {
 
     masonaryResize();
   }
+
+  window.renderResources = renderResources
 
   loadResources();
 })

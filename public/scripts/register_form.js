9d9e6d93b901eaa51c859e7f56f03d8925641099
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-09 02:14:17 +0000

    styled error msgs

--- public/scripts/register_form.js
+++ public/scripts/register_form.js
@@ -1,41 +1,25 @@
 $(function () {
 
-  const showRegisterError = function(message) {
-    $(".register_error").append(`<p>${message}</p>`);
-    $(".register_error").slideDown("slow", function() {});
-  }
-
-  const clearRegisterError = function() {
-    $(".register_error").empty();
-    $(".register_error").css('display', 'none');
-  }
-
-  const clearRegisterInput = function () {
-    $('.modal-register-content').find('input').each(function() {
-      $(this).val('');
-    })
-  }
-
   $('main').on('submit', '.modal-register-form', function(event){
     event.preventDefault();
-    clearRegisterError();
+    clearError('register');
     const data = $(this).serialize();
     signUp(data)
     .done(res => {
       logIn(res)
       $('.modal-bg4').removeClass('bg-active');
-      clearRegisterInput();
+      clearInput('register');
     })
-    .fail(err => showRegisterError(err.responseText))
+    .fail(err => showError(err.responseText, 'register'))
   });
 
   $('.modal-bg4').on('focus','input', function() {
-    clearRegisterError();
+    clearError('register');
   })
 
   $('.modal-register-close').on('click', function() {
-    clearRegisterInput();
-    clearRegisterError();
+    clearInput('register');
+    clearError('register');
   })
 
 });

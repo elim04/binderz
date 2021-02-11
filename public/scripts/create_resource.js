2c46ada17dd380fb8801f5f26d622d26e99cce74
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-02-11 14:03:15 -0500

    check if url exists before creating resource

--- public/scripts/create_resource.js
+++ public/scripts/create_resource.js
@@ -1,15 +1,28 @@
 $(function () {
-  $('#image-url').on('input', function(){
+  $('#image-url').on('input', function () {
     $('.modal-img').empty()
     $('.modal-img').append(`<img class="create-resource-img" src=${$(this).val()}/>`)
   })
 
   $('.create-resource-info').on('submit', function (event) {
     event.preventDefault();
-    $('.modal-bg2').removeClass('bg-active');
-    const data = $(this).serialize();
-    createResource(data);
-    $("#resource-modal-topic").val("none");
-    resetImg();
+
+      $.ajax({
+        url: '/api/resources/urlTest',
+        method: 'POST',
+        data: $('#resource-url').serialize()
+      })
+      .done((res) => {
+        if(res.statusCode === 200){
+          $('.modal-bg2').removeClass('bg-active');
+          const data = $(this).serialize();
+          createResource(data);
+          $("#resource-modal-topic").val("none");
+          resetImg();
+        }
+      })
+      .fail((res) => {
+        alert('Please make sure to use a valid url!')
+      })
   });
-});
+})

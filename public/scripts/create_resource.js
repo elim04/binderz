0b9c507e8b4ba7be7e4fc9a7ebe2473f65815519
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-02-10 20:52:07 -0500

    change image on image src load

--- public/scripts/create_resource.js
+++ public/scripts/create_resource.js
@@ -1,9 +1,15 @@
 $(function () {
+  $('#image-url').on('input', function(){
+    $('.modal-img').empty()
+    $('.modal-img').append(`<img class="create-resource-img" src=${$(this).val()}/>`)
+  })
+
   $('.create-resource-info').on('submit', function (event) {
     event.preventDefault();
     $('.modal-bg2').removeClass('bg-active');
     const data = $(this).serialize();
     createResource(data);
-    $("#resource-modal-topic").val("none")
+    $("#resource-modal-topic").val("none");
+    resetImg();
   });
 });
